a2dccec7cc536fe7b47825f79759c9b9f86159c6
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2023-08-11 11:00:27 +0200

    update: database/db-modeling add table adoption, update: database/schema.sql to add SQL Script based on modeling

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,85 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
-);
+DROP DATABASE IF EXISTS `adopt_an_animal`;
+
+CREATE DATABASE IF NOT EXISTS `adopt_an_animal`;
+
+USE `adopt_an_animal`;
+
+DROP TABLE IF EXISTS `favorite`;
+
+DROP TABLE IF EXISTS `comment`;
+
+DROP TABLE IF EXISTS `adoption`;
+
+DROP TABLE IF EXISTS `animal`;
+
+DROP TABLE IF EXISTS `user`;
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE
+    `category` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(50) NOT NULL,
+        `image` VARCHAR(255) NOT NULL,
+        `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`)
+    );
+
+CREATE TABLE
+    `user` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `firstname` VARCHAR(50) NOT NULL,
+        `lastname` VARCHAR(50) NOT NULL,
+        `email` VARCHAR(255) NOT NULL,
+        `password` VARCHAR(255) NOT NULL,
+        `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`)
+    );
+
+CREATE TABLE
+    `animal` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(255) NOT NULL,
+        `image` VARCHAR(255) NOT NULL,
+        `description` TEXT NOT NULL,
+        `age` INT NOT NULL,
+        `is_adopted` TINYINT(1) NOT NULL DEFAULT 0,
+        `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `category_id` INT NOT NULL,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    );
+
+CREATE TABLE
+    `adoption` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `user_id` INT NOT NULL,
+        `animal_id` INT NOT NULL,
+        `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+        FOREIGN KEY (`animal_id`) REFERENCES `animal` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    );
+
+CREATE TABLE
+    `comment` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `user_id` INT NOT NULL,
+        `animal_id` INT NOT NULL,
+        `comment` TEXT NOT NULL,
+        `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+        FOREIGN KEY (`animal_id`) REFERENCES `animal` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    );
+
+CREATE TABLE
+    `favorite` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `user_id` INT NOT NULL,
+        `animal_id` INT NOT NULL,
+        `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+        FOREIGN KEY (`animal_id`) REFERENCES `animal` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
